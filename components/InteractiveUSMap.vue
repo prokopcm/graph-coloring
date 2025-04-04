32bec6e1c1cea3f2835d94cb10738ff4be6a21dc
Michael Prokopchuk <prokopcm@gmail.com>
2025-04-03 22:03:54 -0400

    Refactor mode code into mapData, rename some variables to improve clarity

--- components/InteractiveUSMap.vue
+++ components/InteractiveUSMap.vue
@@ -1,64 +1,18 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
-import { mapData, stateAbbrevToName } from '~/data/mapData'
+import { adjacentNeighbors, mapData, stateAbbrevToName } from '~/data/mapData'
 import type { GraphNode } from '~/utils/graphUtils'
 
-interface StateColor {
-  [key: string]: string
+interface MapColoring {
+  [key: keyof typeof stateAbbrevToName]: string
 }
 
-const stateColors = ref<StateColor>({
-  AL: '#FFFFFF',
-  AK: '#FFFFFF',
-  AZ: '#FFFFFF',
-  AR: '#FFFFFF',
-  CA: '#FFFFFF',
-  CO: '#FFFFFF',
-  CT: '#FFFFFF',
-  DE: '#FFFFFF',
-  FL: '#FFFFFF',
-  GA: '#FFFFFF',
-  HI: '#FFFFFF',
-  ID: '#FFFFFF',
-  IL: '#FFFFFF',
-  IN: '#FFFFFF',
-  IA: '#FFFFFF',
-  KS: '#FFFFFF',
-  KY: '#FFFFFF',
-  LA: '#FFFFFF',
-  ME: '#FFFFFF',
-  MD: '#FFFFFF',
-  MA: '#FFFFFF',
-  MI: '#FFFFFF',
-  MN: '#FFFFFF',
-  MS: '#FFFFFF',
-  MO: '#FFFFFF',
-  MT: '#FFFFFF',
-  NE: '#FFFFFF',
-  NV: '#FFFFFF',
-  NH: '#FFFFFF',
-  NJ: '#FFFFFF',
-  NM: '#FFFFFF',
-  NY: '#FFFFFF',
-  NC: '#FFFFFF',
-  ND: '#FFFFFF',
-  OH: '#FFFFFF',
-  OK: '#FFFFFF',
-  OR: '#FFFFFF',
-  PA: '#FFFFFF',
-  RI: '#FFFFFF',
-  SC: '#FFFFFF',
-  SD: '#FFFFFF',
-  TN: '#FFFFFF',
-  TX: '#FFFFFF',
-  UT: '#FFFFFF',
-  VT: '#FFFFFF',
-  VA: '#FFFFFF',
-  WA: '#FFFFFF',
-  WV: '#FFFFFF',
-  WI: '#FFFFFF',
-  WY: '#FFFFFF',
-})
+const _mapColoring: MapColoring = {}
+for (const abbrev in stateAbbrevToName) {
+  _mapColoring[abbrev] = '#FFFFFF'
+}
+
+const mapColoring = ref<MapColoring>(_mapColoring)
 
 const selectedState = ref<HTMLElement | null>(null)
 const showColorPicker = ref(false)
@@ -69,7 +23,7 @@ const colorPickerY = ref(0)
 const mouseoverState = ref<HTMLElement | null>(null)
 
 const uncoloredStates = computed(() => {
-  const uncolored = Object.entries(stateColors.value)
+  const uncolored = Object.entries(mapColoring.value)
     .filter(([_, color]) => color === '#FFFFFF')
     .map(([state, _]) => state)
 
@@ -85,13 +39,13 @@ function getFillColor (stateId: string) {
     return '#CCFFEE'
   }
 
-  return stateColors.value[stateId] || '#FFFFFF'
+  return mapColoring.value[stateId] || '#FFFFFF'
 }
 
 function mouseOverState (event: MouseEvent) {
   const stateElement = event.target as HTMLElement
 
-  if (stateElement.tagName === 'path' && stateElement.id && stateColors.value[stateElement.id] === '#FFFFFF') {
+  if (stateElement.tagName === 'path' && stateElement.id && mapColoring.value[stateElement.id] === '#FFFFFF') {
     mouseoverState.value = stateElement
   }
 }
@@ -104,8 +58,8 @@ function mouseOutState (event: MouseEvent) {
       console.log('mouseout same as selected state', stateElement.id)
       return
     }
-    if (stateColors.value[stateElement.id]) {
-      stateElement.style.fill = stateColors.value[stateElement.id]
+    if (mapColoring.value[stateElement.id]) {
+      stateElement.style.fill = mapColoring.value[stateElement.id]
       console.log('mouseout Filling state', stateElement.id)
       return
     }
@@ -119,7 +73,7 @@ function resetFillColor (event: MouseEvent) {
   const stateElement = event.target as HTMLElement
 
   if (stateElement.tagName === 'path' && stateElement.id) {
-    stateColors.value[stateElement.id] = '#FFFFFF'
+    mapColoring.value[stateElement.id] = '#FFFFFF'
     // stateElement.style.fill = '#FFFFFF'
   }
 }
@@ -129,7 +83,7 @@ function stateClicked (event: MouseEvent) {
 
   if (stateElement.tagName === 'path' && stateElement.id) {
     if (selectedState.value && selectedState.value.id !== stateElement.id) {
-      selectedState.value.style.fill = stateColors.value[selectedState.value.id] || '#FFFFFF'
+      selectedState.value.style.fill = mapColoring.value[selectedState.value.id] || '#FFFFFF'
     }
 
     // get page width
@@ -155,7 +109,7 @@ function toggleColorPicker (show: boolean) {
     }
   } else {
     if (selectedState.value) {
-      selectedState.value.style.fill = stateColors.value[selectedState.value.id] || '#FFFFFF'
+      selectedState.value.style.fill = mapColoring.value[selectedState.value.id] || '#FFFFFF'
       selectedState.value = null
     }
 
@@ -170,7 +124,7 @@ function selectState (state: string) {
 
   if (stateElement && stateElement.tagName === 'path') {
     if (selectedState.value && selectedState.value.id !== state) {
-      selectedState.value.style.fill = stateColors.value[selectedState.value.id] || '#FFFFFF'
+      selectedState.value.style.fill = mapColoring.value[selectedState.value.id] || '#FFFFFF'
     }
 
     // Position the color picker near the state element
@@ -187,8 +141,8 @@ function selectState (state: string) {
 }
 
 function resetStateColors () {
-  for (const state in stateColors.value) {
-    stateColors.value[state] = '#FFFFFF'
+  for (const state in mapColoring.value) {
+    mapColoring.value[state] = '#FFFFFF'
   }
   selectedState.value = null
   showColorPicker.value = false
@@ -206,7 +160,7 @@ function resetStateColors () {
 function setColor (color: string) {
   if (selectedState.value) {
     selectedState.value.style.fill = color
-    stateColors.value[selectedState.value.id] = color
+    mapColoring.value[selectedState.value.id] = color
   }
 
   toggleColorPicker(false)
@@ -216,125 +170,6 @@ function mapWrapperClicked (event: MouseEvent) {
   if (event.target instanceof HTMLElement && event.target.id === 'map-wrapper') {
     toggleColorPicker(false)
   }
-}
-
-
-
-const graph = {
-  AL: { name: 'AL', neighbors: ['FL', 'GA', 'MS', 'TN'] },
-  AK: { name: 'AK', neighbors: [] },
-  AZ: { name: 'AZ', neighbors: ['CA', 'CO', 'NV', 'NM', 'UT'] },
-  AR: { name: 'AR', neighbors: ['LA', 'MS', 'MO', 'OK', 'TN', 'TX'] },
-  CA: { name: 'CA', neighbors: ['AZ', 'NV', 'OR'] },
-  CO: {
-    name: 'CO',
-    neighbors: ['AZ', 'KS', 'NM', 'NE', 'OK', 'UT', 'WY'],
-  },
-  CT: { name: 'CT', neighbors: ['MA', 'NY', 'RI'] },
-  DE: { name: 'DE', neighbors: ['MD', 'NJ', 'PA'] },
-  FL: { name: 'FL', neighbors: ['AL', 'GA'] },
-  GA: { name: 'GA', neighbors: ['AL', 'FL', 'NC', 'SC', 'TN'] },
-  HI: { name: 'HI', neighbors: [] },
-  ID: { name: 'ID', neighbors: ['MT', 'NV', 'OR', 'UT', 'WA', 'WY'] },
-  IL: { name: 'IL', neighbors: ['IN', 'IA', 'KY', 'MO', 'WI'] },
-  IN: { name: 'IN', neighbors: ['IL', 'KY', 'MI', 'OH'] },
-  IA: { name: 'IA', neighbors: ['IL', 'MN', 'MO', 'NE', 'SD', 'WI'] },
-  KS: { name: 'KS', neighbors: ['CO', 'MO', 'NE', 'OK'] },
-  KY: {
-    name: 'KY',
-    neighbors: ['IL', 'IN', 'MO', 'OH', 'TN', 'VA', 'WV'],
-  },
-  LA: { name: 'LA', neighbors: ['AR', 'MS', 'TX'] },
-  ME: { name: 'ME', neighbors: ['NH'] },
-  MD: { name: 'MD', neighbors: ['DE', 'PA', 'VA', 'WV'] },
-  MA: { name: 'MA', neighbors: ['CT', 'NH', 'NY', 'RI', 'VT'] },
-  MI: { name: 'MI', neighbors: ['IN', 'OH', 'WI'] },
-  MN: { name: 'MN', neighbors: ['IA', 'ND', 'WI', 'SD'] },
-  MS: { name: 'MS', neighbors: ['AL', 'AR', 'LA', 'TN'] },
-  MO: {
-    name: 'MO',
-    neighbors: ['AR', 'IL', 'IA', 'KS', 'KY', 'NE', 'OK', 'TN'],
-  },
-  MT: { name: 'MT', neighbors: ['ID', 'ND', 'SD', 'WY'] },
-  NE: { name: 'NE', neighbors: ['CO', 'IA', 'KS', 'MO', 'SD', 'WY'] },
-  NV: { name: 'NV', neighbors: ['AZ', 'CA', 'ID', 'OR', 'UT'] },
-  NH: { name: 'NH', neighbors: ['ME', 'MA', 'VT'] },
-  NJ: { name: 'NJ', neighbors: ['DE', 'NY', 'PA'] },
-  NM: { name: 'NM', neighbors: ['AZ', 'CO', 'OK', 'TX', 'UT'] },
-  NY: { name: 'NY', neighbors: ['CT', 'MA', 'NJ', 'PA', 'VT'] },
-  NC: { name: 'NC', neighbors: ['GA', 'SC', 'TN', 'VA'] },
-  ND: { name: 'ND', neighbors: ['MN', 'MT', 'SD'] },
-  OH: { name: 'OH', neighbors: ['IN', 'KY', 'MI', 'PA', 'WV'] },
-  OK: { name: 'OK', neighbors: ['AR', 'CO', 'KS', 'MO', 'NM', 'TX'] },
-  OR: { name: 'OR', neighbors: ['CA', 'ID', 'NV', 'WA'] },
-  PA: { name: 'PA', neighbors: ['DE', 'MD', 'NY', 'NJ', 'OH', 'WV'] },
-  RI: { name: 'RI', neighbors: ['CT', 'MA'] },
-  SC: { name: 'SC', neighbors: ['GA', 'NC'] },
-  SD: { name: 'SD', neighbors: ['IA', 'MN', 'MT', 'ND', 'NE', 'WY'] },
-  TN: {
-    name: 'TN',
-    neighbors: ['AL', 'AR', 'KY', 'GA', 'MO', 'MS', 'NC', 'VA'],
-  },
-  TX: { name: 'TX', neighbors: ['AR', 'LA', 'NM', 'OK'] },
-  UT: { name: 'UT', neighbors: ['AZ', 'CO', 'ID', 'NV', 'NM', 'WY'] },
-  VT: { name: 'VT', neighbors: ['NY', 'NH', 'MA'] },
-  VA: { name: 'VA', neighbors: ['KY', 'MD', 'NC', 'TN', 'WV'] },
-  WA: { name: 'WA', neighbors: ['ID', 'OR'] },
-  WV: { name: 'WV', neighbors: ['KY', 'MD', 'OH', 'PA', 'VA'] },
-  WI: { name: 'WI', neighbors: ['IL', 'IA', 'MI', 'MN'] },
-  WY: { name: 'WY', neighbors: ['CO', 'ID', 'MT', 'NE', 'SD', 'UT'] },
-}
-const states: Record<string, string> = {
-  AL: 'Alabama',
-  AK: 'Alaska',
-  AZ: 'Arizona',
-  AR: 'Arkansas',
-  CA: 'California',
-  CO: 'Colorado',
-  CT: 'Connecticut',
-  DE: 'Delaware',
-  FL: 'Florida',
-  GA: 'Georgia',
-  HI: 'Hawaii',
-  ID: 'Idaho',
-  IL: 'Illinois',
-  IN: 'Indiana',
-  IA: 'Iowa',
-  KS: 'Kansas',
-  KY: 'Kentucky',
-  LA: 'Louisiana',
-  ME: 'Maine',
-  MD: 'Maryland',
-  MA: 'Massachusetts',
-  MI: 'Michigan',
-  MN: 'Minnesota',
-  MS: 'Mississippi',
-  MO: 'Missouri',
-  MT: 'Montana',
-  NE: 'Nebraska',
-  NV: 'Nevada',
-  NH: 'New Hampshire',
-  NJ: 'New Jersey',
-  NM: 'New Mexico',
-  NY: 'New York',
-  NC: 'North Carolina',
-  ND: 'North Dakota',
-  OH: 'Ohio',
-  OK: 'Oklahoma',
-  OR: 'Oregon',
-  PA: 'Pennsylvania',
-  RI: 'Rhode Island',
-  SC: 'South Carolina',
-  SD: 'South Dakota',
-  TN: 'Tennessee',
-  TX: 'Texas',
-  UT: 'Utah',
-  VT: 'Vermont',
-  VA: 'Virginia',
-  WA: 'Washington',
-  WV: 'West Virginia',
-  WI: 'Wisconsin',
-  WY: 'Wyoming',
 }
 
 function neighborsWithSameColor (graphState: GraphNode): Array<{ name: string, color: string }> {
@@ -351,7 +186,7 @@ function neighborsWithSameColor (graphState: GraphNode): Array<{ name: string, c
         && stateElement.style.fill !== 'rgb(255, 255, 255)'
       ) {
         return {
-          name: states[neighbor],
+          name: stateAbbrevToName[neighbor],
           color: neighborElement.style.fill,
         }
       } else {
@@ -394,12 +229,12 @@ function achievedFourColorTheorem (graph: GraphInterface) {
     .filter(value => value !== null && value !== undefined)
 }
 
-function miscolorStatePresenter (stateColor: StateColor) {
+function miscolorStatePresenter (stateColor: MapColoring) {
   return `<div style="color:${stateColor.color};">${stateColor.name}<div>`
 }
 
 const colorCollidingStates = computed(() => {
-  const innerHTML = achievedFourColorTheorem(graph)
+  const innerHTML = achievedFourColorTheorem(adjacentNeighbors)
     .map(miscolorStatePresenter)
     .join('')
   const remainingColorCollidingStates = document.getElementById('color-colliding-states')
