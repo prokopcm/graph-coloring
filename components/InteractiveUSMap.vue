1df24435913a460623db48dc786fd58e7caabb85
Michael Prokopchuk <prokopcm@gmail.com>
2025-04-05 13:03:59 -0400

    Add explicit button styling to components just to make sure buttons get styled right on ios

--- components/InteractiveUSMap.vue
+++ components/InteractiveUSMap.vue
@@ -515,4 +515,20 @@ onMounted(() => {
 li {
   display: list-item;
 }
+
+.link-button {
+  display: inline-block;
+  border: 1px solid #000;
+  background: #FFF;
+  color: #000;
+  border-radius: 100px;
+  transition: color 0.1s ease, background 0.1s ease;
+  padding: 12px 24px;
+}
+
+.link-button:hover {
+  background: #333;
+  color: #FFF;
+  transition: color .4s ease, background .7s ease;
+}
 </style>
