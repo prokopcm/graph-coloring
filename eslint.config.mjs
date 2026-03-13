4dffcd3912a11dc41e2cc3c21a1f3b115cdc9038
Michael Prokopchuk <prokopcm@gmail.com>
2026-03-12 21:50:07 -0400

    style: clean up eslint config overrides in favor of antfu defaults

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,20 +12,6 @@ export default withNuxt(
     },
     rules: {
       'antfu/consistent-chaining': 'off',
-      'ts/no-unsafe-argument': 'off',
-      'ts/no-unsafe-assignment': 'off',
-      'ts/no-unsafe-call': 'off',
-      'ts/no-unsafe-member-access': 'off',
-      'ts/no-unsafe-return': 'off',
-      'unused-imports/no-unused-imports': 'warn',
-      'unused-imports/no-unused-vars': 'warn',
-      'vue/no-multiple-template-root': 'off',
-      'vue/static-class-names-order': ['error'],
-      'space-before-function-paren': ['error', 'always'],
-      'vue/no-irregular-whitespace': 'off',
-      '@/space-before-function-paren': ['error', 'always'],
-      'style/space-before-function-paren': ['error', 'always'],
-      'brace-style': ['error', '1tbs'],
     },
     // https://github.com/antfu/eslint-config
     typescript: {
